b6b8329548c5b806fdc7a3730be0c4659732a0ba
Ma-ishii <b52keeptype@gmail.com>
2018-12-01 20:30:21 +0900

    aa

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get    '/signup',  to: 'users#new'
   post   '/signup',  to: 'users#create'
-  get    '/edit',    to: 'users#edit'
+
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
